0b27754fabe546bc1a6646f6e1736af96be9effb
jtrotsky <5625795+jtrotsky@users.noreply.github.com>
2016-01-07 09:18:10 +1300

    Changes to readme and main

--- vendapi/vend.go
+++ vendapi/vend.go
@@ -9,7 +9,6 @@ import (
 
 // SalePage grabs and collates all sales.
 func SalePage(v int64, domainPrefix, token string) ([]vend.Sale, int64, error) {
-
 	var s, sales []vend.Sale
 
 	// v is a version that is used to objects by page.
